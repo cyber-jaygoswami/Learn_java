6e00651fe7c2b9849007527b20baeca3ed4bd853
jay <jaygoswami600@gmail.com>
2025-09-15 14:15:06 +0530

    learn 1 table per hierarchy class inheritance

---
+++ Spring/Chpater-04/inheritance1/src/main/java/com/learnJava/inheritance1/App.java
@@ -0,0 +1,46 @@
+package com.learnJava.inheritance1;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Regular_Employee re = new Regular_Employee();
+    	re.setId(1);
+    	re.setName("Jay");
+    	re.setSalary(50000);
+    	re.setBonus(10000);
+    	
+    	Contract_Employee ce = new Contract_Employee();
+    	ce.setId(2);
+    	ce.setName("Ajay");
+    	ce.setPay_per_hour(200);
+    	ce.setContract_period("6 Months");
+    	
+    	Employee emp1 = new Employee();
+    	emp1.setId(3);
+    	emp1.setName("Hitesh");
+    	
+    	
+        Configuration conf = new Configuration().configure();
+        SessionFactory sf = conf.buildSessionFactory();
+        Session session = sf.openSession();
+        
+        session.beginTransaction();
+        
+        
+        // CRUD Operation
+//        session.save(re);
+        session.save(ce);
+        session.save(emp1);
+        
+        session.getTransaction().commit();
+    }
+}
